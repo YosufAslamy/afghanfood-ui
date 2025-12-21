b56741767505a29b9365f88ede1ad5646e50e32c
YosufAslamy <yosufaslamy9@gmail.com>
2025-12-21 22:51:40 +0430

    Fetch the foods

--- src/admin_panel/pages/DataTable.jsx
+++ src/admin_panel/pages/DataTable.jsx
@@ -83,18 +83,23 @@ export default function DataTables() {
 }
 
 // List Of Foods On Menu Page!!!
-const foods = [
-  {
-    id: 1,
-    category_id: 1,
-    name: "Kabuli Pulao",
-    description: "A traditional Afghan rice dish with lamb and raisins.",
-    price: "$12",
-    is_vegetarian: false,
-  },
-];
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function FoodTable() {
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/foods")
+      .then((response) => {
+        setFoods(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching foods:", error);
+      });
+  }, []);
+
   return (
     <Table>
       <TableHeader>
@@ -102,27 +107,11 @@ export function FoodTable() {
           <TableHead>ID</TableHead>
           <TableHead>Category Id</TableHead>
           <TableHead>Name</TableHead>
-          <TableHead>Describtion</TableHead>
+          <TableHead>Description</TableHead>
           <TableHead>Price</TableHead>
           <TableHead>Photo Url</TableHead>
           <TableHead>Vegetarian</TableHead>
-
-          <TableCell className="text-right">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="h-8 w-8 p-0">
-                  <MoreHorizontal />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>Add</DropdownMenuLabel>
-                <DropdownMenuItem>Update</DropdownMenuItem>
-                <DropdownMenuItem className="text-red-600">
-                  Delete
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </TableCell>
+          <TableHead>Active</TableHead>
         </TableRow>
       </TableHeader>
 
@@ -133,8 +122,10 @@ export function FoodTable() {
             <TableCell>{row.category_id}</TableCell>
             <TableCell>{row.name}</TableCell>
             <TableCell>{row.description}</TableCell>
-            <TableCell>{row.price}</TableCell>
+            <TableCell>${row.price}</TableCell>
+            <TableCell>{row.photo_url}</TableCell>
             <TableCell>{row.is_vegetarian ? "Yes" : "No"}</TableCell>
+            <TableCell>{row.is_active ? "Active" : "Inactive"}</TableCell>
           </TableRow>
         ))}
       </TableBody>
